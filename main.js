77523b3d08419bbcbb527f205d666c026f03c34d
Teitei Wisdom <120546016+Ajinormotor@users.noreply.github.com>
2023-09-08 18:47:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const slackUserName = document.querySelector(' [data-testid="slackUsername"]');
+const slackUserName = document.querySelector(' [data-testid="slackUserName"]');
 slackUserName.textContent = "Teitei Wisdom";
 
 
 
-const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]')
-
- currentDay.textContent = new Date().toLocaleDateString({weekday: 'long'});
+const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+const options = { weekday: 'long' };
+currentDay.textContent = new Date().toLocaleDateString('en-US', options);
 
 
 
@@ -15,7 +15,8 @@ setInterval(()=>{
 currentUTCime.textContent = Date.now();
 }, 1000)
 
-
+const githubURL = document.querySelector('[data-testid="githubURL"]');
+githubURL.href = "https://github.com/Ajinormotor"; 
 
 
 
